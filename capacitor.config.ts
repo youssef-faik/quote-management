06a2937958712068d49af0c1d046b1107470112b
Youssef Faik <faik.yusef@gmail.com>
2025-04-29 12:49:14 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'quote-management',
+  webDir: 'www'
+};
+
+export default config;
